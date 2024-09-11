5ccee8a90ce08d345d46760d2b07d43db86d771c
unknown <l.jungkunz@tum.de>
2024-09-11 05:37:42 +0200

    node keeps running and writing to the file :3

--- FuckingNode/index.js
+++ FuckingNode/index.js
@@ -1,5 +1,6 @@
 import { ZkSendLinkBuilder, ZkSendLink } from '@mysten/zksend';
 import * as fs from 'fs';
+import * as NodeSchedule from 'node-schedule';
 
 let codes = [];
 codes.push("https://getstashed.com/claim#$8A8Krv00c9+2qJhZ1+04GS6J+9VXZhfTMWkHRA6129A=");
@@ -8,8 +9,21 @@ codes.push("https://getstashed.com/claim#$MVpMofoXBmLeZYpUklHZAw03TrC/ENRd0DFPT5
 codes.push("https://getstashed.com/claim#$nH+1elBUBKXAEqjLqrNw3Y79ayutenT5lrRg/PnwFo8=");
 
 // TODO: real links
+let counter = 0;
 
-
+NodeSchedule.scheduleJob('*/1 * * * * *', async () => {
+    let rink = [];
+    for (let i = 0; i < codes.length; i++) {
+        let tlink = await ZkSendLink.fromUrl(codes[i]);
+        rink.push({link:codes[i], claimed:tlink.claimed})
+    }
+    console.log(rink);
+    fs.writeFileSync('R:/claims.json', JSON.stringify(rink));
+    counter++;
+    console.log(counter);
+    
+})
+/*
 let rink = [];
 for (let i = 0; i < codes.length; i++) {
     let tlink = await ZkSendLink.fromUrl(codes[i]);
@@ -17,4 +31,4 @@ for (let i = 0; i < codes.length; i++) {
 }
 console.log(rink);
 fs.writeFileSync('R:/claims.json', JSON.stringify(rink));
-
+*/
