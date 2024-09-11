145e1120aceb3fd0e182c3a2c6782db6d389bf53
unknown <l.jungkunz@tum.de>
2024-09-12 01:28:56 +0200

    zeug was wir im Dome gemacht haben

---
+++ Assets/fps.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fps : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 30;
+        Debug.Log("Help");
+    }
+
+
+    void Awake()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
