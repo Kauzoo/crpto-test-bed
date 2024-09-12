be97b070f8fbf5daaa988601ccfc01f48de88af5
nyr <kauzoo@web.de>
2024-09-12 17:08:56 +0200

    pls dont break

--- Assets/Scenes/City/Scripts/Despawner.cs
+++ Assets/Scenes/City/Scripts/Despawner.cs
@@ -1,18 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Text.Json;
-using System.Threading.Tasks;
-using Palmmedia.ReportGenerator.Core.Common;
 using UnityEngine;
 
 namespace Scenes.City.Scripts
 {
     public class Despawner : MonoBehaviour
     {
-        public string jsonPath = @"R:\testclaim.json";
         private FileStream _fileStream;
         private StreamReader _streamReader;
+        private Dictionary<string, bool> _itemDictionary = new();
+        //private Dictionary<string, int> _lookupTable = new();
+        private Dictionary<string, SpriteRenderer[]> _itemObjectDictionary = new();
+        private const int NAME_OFFSET = 30;
+        
+        
+        [Header("Settings")]
+        public string jsonPath = @"R:\testclaim.json";
+        public Transform[] qrcodes = new Transform[1];
 
         private void Awake()
         {
@@ -34,12 +39,68 @@ namespace Scenes.City.Scripts
 
         private void Start()
         {
-            ParseJSON();
+            Application.targetFrameRate = 30;
+            Setup();
+            //ParseJSON();
+            foreach (var entry in _itemObjectDictionary)
+            {
+                Debug.Log($"object keys: {entry.Key}");
+            }
         }
 
         void Update()
         {
+            CycleJSON();
+        }
+
+        public void Setup()
+        {
+            // Initialize
+            // Setup initial dictionary
+            var content = _streamReader.ReadToEnd();
+            content = content.Trim('[');
+            content = content.Trim(']');
+            var items = content.Split(",", StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < items.Length; i += 2)
+            {
+                var link = items[i].Substring(items[i].IndexOf(':') + 1).Trim('"');
+                link = link.Trim('{');
+                link = link.Trim('}');
+                var unique_id = link[30..].Trim('"');
+                Debug.Log($"ID {i}: {unique_id} | Length: {unique_id.Length}");
+                var is_claimed = bool.Parse(items[i + 1].Split(':')[1].Trim('}'));
+                _itemDictionary.Add(unique_id, is_claimed);
+            }
             
+            // Fill id to sprite map
+            foreach (var entry in _itemDictionary)
+            {
+                foreach (var code in qrcodes)
+                {
+                    var sprites = code.GetComponentsInChildren<SpriteRenderer>();
+                    Debug.Log($"Sprite Name: {sprites[0].sprite.name}");
+                    if (sprites[0].sprite.name == entry.Key.Replace('/', '€'))
+                    {
+                        _itemObjectDictionary.Add(entry.Key, sprites);
+                    }
+                }
+            }
+            
+        }
+        
+        public void CycleJSON()
+        {
+            ParseJSON();
+            foreach (var item in _itemObjectDictionary)
+            {
+                // Removed QR-codes that have been claimed
+                if (!_itemDictionary.TryGetValue(item.Key.Replace('€', '/'), out var is_claimed)) continue;
+                if (is_claimed)
+                {
+                    FadeOutQR(item.Key);
+                    _itemObjectDictionary.Remove(item.Key);
+                }
+            }
         }
 
         public void ParseJSON()
@@ -48,23 +109,35 @@ namespace Scenes.City.Scripts
             content = content.Trim('[');
             content = content.Trim(']');
             var items = content.Split(",", StringSplitOptions.RemoveEmptyEntries);
-            Dictionary<string, bool> itemDictionary = new Dictionary<string, bool>();
             for (int i = 0; i < items.Length; i += 2)
             {
                 var link = items[i].Substring(items[i].IndexOf(':') + 1).Trim('"');
                 link = link.Trim('{');
                 link = link.Trim('}');
+                var unique_id = link[30..].Trim('"');
+                //Debug.Log($"ID {i}: {unique_id} | Length: {unique_id.Length}");
                 var is_claimed = bool.Parse(items[i + 1].Split(':')[1].Trim('}'));
-                itemDictionary.Add(link, is_claimed);
-            }
-
-            foreach (var entry in itemDictionary)
-            {
-                if (entry.Value == true)
+                if (_itemDictionary.ContainsKey(unique_id))
                 {
-                    Debug.Log(entry.Key);
+                    _itemDictionary[unique_id] = is_claimed;
                 }
             }
+        }
+
+        public void SetupJSONLookupTable()
+        {
+            // TODO Implement a more efficient lookup table based way to parse the json file
+            throw new NotImplementedException();
+        }
+
+        public void FadeOutQR(string id)
+        {
+            var sprites = _itemObjectDictionary[id];
+            foreach (var sprite in sprites)
+            {
+                sprite.enabled = false;
+            }
+            Debug.Log($"Code {id} was claimed");
         }
     }
 }
