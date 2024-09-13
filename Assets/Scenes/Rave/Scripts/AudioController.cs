4b4a08166972e55e82eccdc4cce370fae323134e
unknown <l.jungkunz@tum.de>
2024-09-13 12:48:47 +0200

    lunchtime

--- Assets/Scenes/Rave/Scripts/AudioController.cs
+++ Assets/Scenes/Rave/Scripts/AudioController.cs
@@ -6,7 +6,7 @@ using AudioVisualizer;
 public class AudioController : MonoBehaviour
 {
     public AudioSpectrumEngine audioSpectrumEngine;
-    public Transform pyramidTransform;
+    //public Transform pyramidTransform;
 
     /*public Transform[] ob = new Transform[8];*/
     
@@ -19,10 +19,12 @@ public class AudioController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /*
         if (audioSpectrumEngine.GetBeat())
         {
             pyramidTransform.Rotate(Vector3.up, 5);
         }
+        */
 
         var bands = audioSpectrumEngine.GetBands();
         foreach (var val in bands)
